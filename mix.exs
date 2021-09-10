f73c12da4fc60fd0c1052c61e7b7c9489a277f67
Alex Drummond <a.d.drummond@gmail.com>
2021-09-10 18:03:01 +0200

    fix annoying phoenix config warning

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule EexToHeex.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:html_entities, "~> 0.5"},
-      {:phoenix_live_view, "~> 0.16.0"},
+      {:phoenix_live_view, "~> 0.16.0", runtime: false},
       {:briefly, "~> 0.3"}
     ]
   end
