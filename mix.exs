504a00268524c2f60aa9eb22dfa6d04adf950c94
Alex Drummond <a.d.drummond@gmail.com>
2024-03-07 21:18:10 +0000

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EexToHeex.MixProject do
   def project do
     [
       app: :eextoheex,
-      version: "0.1.5",
+      version: "0.2.5",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
